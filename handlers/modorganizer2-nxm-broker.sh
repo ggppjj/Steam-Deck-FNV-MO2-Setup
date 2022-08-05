32c79889385dc1f3cced0943eb04f8d399073202
rockerbacon <vitor.santos@lab309.com.br>
2022-08-05 10:31:50 -0300

    Added separate flow to differentiate flatpak installs on download handler

--- handlers/modorganizer2-nxm-broker.sh
+++ handlers/modorganizer2-nxm-broker.sh
@@ -28,15 +28,55 @@ process_search_status=$?
 
 game_appid=$(cat "$instance_dir/appid.txt")
 
+function send_download_to_running_instance() {
+	if [ -z "$(command -v protontricks)" ]; then
+		if [ -n "$(command -v flatpak)" ]; then
+			if flatpak info com.github.Matoking.protontricks > /dev/null; then
+				using_flatpak_protontricks=1
+			fi
+		fi
+	else
+		using_flatpak_protontricks=0
+	fi
+
+	executable_path="$instance_dir/modorganizer2/nxmhandler.exe"
+	if [ "$using_flatpak_protontricks" == "0"]; then
+		WINEESYNC=1 WINEFSYNC=1 protontricks-launch --appid "$game_appid" "$executable_path" "$nxm_link"
+	else
+		flatpak run --env='WINEESYNC=1' --env='WINEFSYNC=1' --command=protontricks-launch com.github.Matoking.protontricks --appid "$game_appid" "$executable_path" "$nxm_link"
+	fi
+
+	return $?
+}
+
+function start_download() {
+	if [ -z "$(command -v steam)" ]; then
+		if [ -n "$(command -v flatpak)" ]; then
+			if flatpak info com.valvesoftware.Steam > /dev/null; then
+				using_flatpak_steam=1
+			fi
+		fi
+	else
+		using_flatpak_steam=0
+	fi
+
+	if [ "$using_flatpak_steam" == "0"]; then
+		steam -applaunch "$game_appid" "$nxm_link"
+	else
+		flatpak run com.valvesoftware.Steam -applaunch "$game_appid" "$nxm_link"
+	fi
+
+	return $?
+}
+
 if [ "$process_search_status" == "0" ]; then
 	echo "INFO: sending download '$nxm_link' to running Mod Organizer 2 instance"
-	download_start_output=$(WINEESYNC=1 WINEFSYNC=1 protontricks-launch --appid "$game_appid" "$instance_dir/modorganizer2/nxmhandler.exe" "$nxm_link")
-	download_start_status=$?
+	download_start_output=$(send_download_to_running_instance 2>&1)
 else
 	echo "INFO: starting Mod Organizer 2 to download '$nxm_link'"
-	download_start_output=$(steam -applaunch "$game_appid" "$nxm_link")
-	download_start_status=$?
+	download_start_output=$(start_download 2>&1)
 fi
+download_start_status=$?
 
 if [ "$download_start_status" != "0" ]; then
 	zenity --ok-label=Exit --ellipsize --error --text \
